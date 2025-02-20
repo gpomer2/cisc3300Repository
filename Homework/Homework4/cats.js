3677e350011a55183375c8a439e13d2af608dedc
George Pomer <georgepomer@MacBook-Pro-5.local>
2025-02-20 16:05:44 -0500

    in class 10 folder

---
+++ Homework/Homework4/cats.js
@@ -0,0 +1,70 @@
+/*
+6. Loop through the cats array variable
+    a. if the cats adoption status is available add that cat’s name to a result array
+
+    b. create a new sentence about your newly adopted cats from your result array via the join array function.
+*/
+
+
+
+
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+const cat = {name:"Pinecone", age:13, type:'Munchkin', cuteness: 9001};
